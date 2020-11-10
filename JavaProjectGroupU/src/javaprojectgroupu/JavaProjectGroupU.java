c0e8ef6c1fd82020bc5297b3c288054cba85b780
Danrlei Martins <danrlei.martins@gmail.com>
2020-11-10 12:48:29 +0000

    Java Project
    Basic Java Project

---
+++ JavaProjectGroupU/src/javaprojectgroupu/JavaProjectGroupU.java
@@ -0,0 +1,17 @@
+/*
+ Module: Time & Task Management
+CA2 - 
+Lecturer: John Snel
+authors: Danrlei da Silva Martins
+ */
+package javaprojectgroupu;
+
+public class JavaProjectGroupU {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello everyone!");
+    }
+    
+}
